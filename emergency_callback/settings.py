2ae3df781734d75c6c6330062ff7d42aada5108a
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2025-07-21 16:24:45 +0500

    static root

--- emergency_callback/settings.py
+++ emergency_callback/settings.py
@@ -128,6 +128,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+] if os.path.exists(os.path.join(BASE_DIR, 'static')) else []
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
