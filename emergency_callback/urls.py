a0d32f208d1b3147dee554f426332adc42f8fad7
Diyarbekoralbaev <diyarbekoralbaev@gmail.com>
2025-10-28 18:56:57 +0500

    style: remove commented-out admin path

--- emergency_callback/urls.py
+++ emergency_callback/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('callbacks.urls', namespace='callbacks')),
     path('users/', include('users.urls', namespace='users')),
     path('teams/', include('teams.urls', namespace='teams')),
